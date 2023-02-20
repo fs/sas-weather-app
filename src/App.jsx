c5b4b1b18e8bd467d5d1cd10187fde50a0446960
V_Shcherbakov <v.shcherbakov.sas@gmail.com>
2023-02-20 16:22:40 +0500

    moved functions to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import TestComponent from "./components/TestComponent";
-import GetLocation from "./functions/get_location";
 
 const App = () => {
+  const [latitude, setLatitude] = useState();
+  const [longitude, setlongitude] = useState();
+
+  const Success = (position) => {
+    setLatitude(position.coords.latitude);
+    setlongitude(position.coords.longitude);
+  };
+
+  const GetLocation = () => {
+    navigator.geolocation.getCurrentPosition(Success);
+  };
+
   useEffect(() => {
     GetLocation();
   }, []);
@@ -10,6 +21,8 @@ const App = () => {
   return (
     <div data-testid="test-weather-container">
       <TestComponent />
+      <p>{longitude}</p>
+      <p>{latitude}</p>
     </div>
   );
 };
