8893bf58abe628359d2fbbd12da717b3611e304e
Elizabeth <elizaveta.chernosheikina@yandex.ru>
2023-02-20 17:10:58 +0300

    add api method to fetch current weather

--- src/components/TestComponent.jsx
+++ src/components/TestComponent.jsx
@@ -1,7 +1,14 @@
 import { TestDiv } from "./TestComponent/styled";
+// import fetchWeather from "../api/fetchWeather";
 
 const TestComponent = () => {
   return <TestDiv>Hello world 2</TestDiv>;
 };
+
+// const latitude = 48.8567;
+// const longitude = 2.3508;
+
+// const result = await fetchWeather({ latitude, longitude });
+// console.log(result);
 
 export default TestComponent;
