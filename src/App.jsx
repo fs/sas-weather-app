f9c005f645864bd96e757026ed62e677319d820a
nadezhdakharchuk <nadezhda.kharchuk@flatstack.com>
2023-02-20 09:39:35 +0300

    fix test

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import TestComponent from "./components/TestComponent";
+
+const App = () => {
+  return (
+    <div data-testid="test-weather-container">
+      <TestComponent />
+    </div>
+  );
+};
+
+export default App;
