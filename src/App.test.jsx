2f4e167836e3add91c995dab9c7ac7cf610432b3
V_Shcherbakov <v.shcherbakov.sas@gmail.com>
2023-02-20 16:30:41 +0500

    added geolocation mock

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,6 +2,11 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders learn react link", () => {
+  const mockGeolocation = {
+    getCurrentPosition: jest.fn(),
+  };
+
+  global.navigator.geolocation = mockGeolocation;
   render(<App />);
   const linkElement = screen.getByTestId("test-weather-container");
   expect(linkElement).toBeInTheDocument();
