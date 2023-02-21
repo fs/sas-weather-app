8f369967638b3bafa76c952264002c6a5528a2b1
V_Shcherbakov <v.shcherbakov.sas@gmail.com>
2023-02-21 21:51:06 +0500

    initiation commit

---
+++ src/components/WeatherContainer/WeatherContainer.jsx
@@ -0,0 +1,5 @@
+const WeatherContainer = () => {
+  return <div>Test</div>;
+};
+
+export default WeatherContainer;
---
+++ src/components/WeatherContainer/styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+const Something = styled.div``;
+
+export { Container, Something };
