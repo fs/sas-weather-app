136eea677bd4b8612087fd8c7f47bddd529bcad1
nadezhdakharchuk <nadezhda.kharchuk@flatstack.com>
2023-02-28 18:50:40 +0300

    fix

--- src/components/IconComponent/styled.js
+++ src/components/IconComponent/styled.js
@@ -4,11 +4,11 @@ export const BackgroundIcon = styled.img`
   position: absolute;
   z-index: -1;
 
-  @media only screen and (max-width: 599px) {
-    background: none;
+  @media only screen and (max-width: 799px) {
+    display: none;
   }
 
-  @media only screen and (min-width: 600px) {
+  @media only screen and (min-width: 800px) {
     right: -120px;
     top: -80px;
     width: 300px;
