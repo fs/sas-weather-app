5d84c235486ca9ed1f8ad88a61b2a7097bbb473e
V_Shcherbakov <v.shcherbakov.sas@gmail.com>
2023-03-01 12:59:18 +0500

    one file did note make it to the previous commit

--- src/components/WeatherCardComponent/WeatherCardComponent.jsx
+++ src/components/WeatherCardComponent/WeatherCardComponent.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/self-closing-comp */
 import SearchBar from "../SearchBar";
 import IconComponent from "../IconComponent";
-import { WeatherCard } from "./styled";
+import WeatherCard from "./styled";
 import WeatherTextBoxComponent from "../WeatherTextBoxComponent";
 
 const WeatherCardComponent = ({ weatherData }) => {
