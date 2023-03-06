4b82875c61b14a5ee580b266ff1a6d1ce4c14887
BiassedTomato <43648634+BiassedTomato@users.noreply.github.com>
2023-03-06 12:51:35 +0500

    Put tests into a test suite

--- src/hooks/useGeolocation.test.js
+++ src/hooks/useGeolocation.test.js
@@ -1,64 +1,68 @@
 import { renderHook } from "@testing-library/react";
 import useGeolocation from "./useGeolocation";
 
-test("Test geolocation when permission granted", () => {
-  const mockGeolocation = {
-    getCurrentPosition: jest.fn().mockImplementation((success) =>
-      Promise.resolve(
-        success({
-          coords: {
-            latitude: 123321,
-            longitude: 123321,
-          },
-        }),
+describe("Geolocation hook tests", () => {
+  test("Test geolocation when permission granted", () => {
+    const mockGeolocation = {
+      getCurrentPosition: jest.fn().mockImplementation((success) =>
+        Promise.resolve(
+          success({
+            coords: {
+              latitude: 123321,
+              longitude: 123321,
+            },
+          }),
+        ),
       ),
-    ),
-  };
+    };
 
-  global.navigator.geolocation = mockGeolocation;
-  global.navigator.permissions = {
-    query: jest
-      .fn()
-      .mockImplementationOnce(() => Promise.resolve({ state: "granted" })),
-  };
+    global.navigator.geolocation = mockGeolocation;
+    global.navigator.permissions = {
+      query: jest
+        .fn()
+        .mockImplementationOnce(() => Promise.resolve({ state: "granted" })),
+    };
 
-  global.navigator.permissions
-    .query({ name: "geolocation" })
-    .then((permissionStatus) => expect(permissionStatus.state).toBe("granted"));
+    global.navigator.permissions
+      .query({ name: "geolocation" })
+      .then((permissionStatus) =>
+        expect(permissionStatus.state).toBe("granted"),
+      );
 
-  const { result } = renderHook(useGeolocation);
+    const { result } = renderHook(useGeolocation);
 
-  expect(result.current).toEqual({
-    latitude: 123321,
-    longitude: 123321,
-    status: "success",
-    error: null,
+    expect(result.current).toEqual({
+      latitude: 123321,
+      longitude: 123321,
+      status: "success",
+      error: null,
+    });
   });
-});
 
-test("Test geolocation when permission denied", () => {
-  const mockGeolocation = {
-    getCurrentPosition: jest.fn().mockImplementation((success, rejected) => {
-      rejected({
-        message: "TEST_MSG",
-      });
-    }),
-  };
+  test("Test geolocation when permission denied", () => {
+    const mockGeolocation = {
+      getCurrentPosition: jest.fn().mockImplementation((success, rejected) => {
+        rejected({
+          message: "TEST_MSG",
+        });
+      }),
+    };
 
-  global.navigator.geolocation = mockGeolocation;
+    global.navigator.geolocation = mockGeolocation;
 
-  global.navigator.permissions = {
-    query: jest
-      .fn()
-      .mockImplementationOnce(() => Promise.resolve({ state: "denied" })),
-  };
+    global.navigator.permissions = {
+      query: jest
+        .fn()
+        .mockImplementationOnce(() => Promise.resolve({ state: "denied" })),
+    };
 
-  const { result } = renderHook(useGeolocation);
+    const { result } = renderHook(useGeolocation);
 
-  expect(result.current).toEqual({
-    latitude: null,
-    longitude: null,
-    status: "error",
-    error: "TEST_MSG",
+    expect(result.current).toEqual({
+      latitude: null,
+      longitude: null,
+      status: "error",
+      error: "TEST_MSG",
+    });
   });
 });
