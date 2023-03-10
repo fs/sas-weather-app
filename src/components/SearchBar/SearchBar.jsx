b8a2849df44202c0b78cee596681f498c1037cc4
ElsaChern <94759165+ElsaChern@users.noreply.github.com>
2023-03-10 10:10:04 +0300

    Add debounce (#25)

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,31 +1,30 @@
 import { Search } from "react-bootstrap-icons";
 import { useState } from "react";
-import AsyncSelect from "react-select/async";
-
+import { AsyncPaginate } from "react-select-async-paginate";
 import fetchCities from "../../api/fetchCities";
 import { SearchDiv, SearchIconContainer, selectStyles } from "./styled";
 
 const SearchBar = () => {
   const [error, setError] = useState(null);
 
-  const loadOptions = async (inputValue, callback) => {
+  const loadOptions = async (inputValue) => {
     if (inputValue !== "") {
       try {
         const result = await fetchCities(inputValue);
         setError(null);
-        callback(result);
+        return { options: result };
       } catch (e) {
         setError(e);
       }
-    } else {
-      callback([]);
     }
+    return { options: [] };
   };
 
   return (
     <>
       <SearchDiv>
-        <AsyncSelect
+        <AsyncPaginate
+          debounceTimeout={350}
           placeholder="Search"
           loadOptions={loadOptions}
           styles={selectStyles}
