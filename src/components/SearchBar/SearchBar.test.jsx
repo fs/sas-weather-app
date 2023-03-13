910d9b490bd0f09a5703c3a2cb82792d208f522e
Elizabeth <elizaveta.chernosheikina@yandex.ru>
2023-03-13 21:54:08 +0300

    add two tests

--- src/components/SearchBar/SearchBar.test.jsx
+++ src/components/SearchBar/SearchBar.test.jsx
@@ -10,11 +10,10 @@ describe("SearchBar", () => {
     // Arrange
     const input = "Moscow";
 
-    const expectedResult = mockSuccessResult;
     const mockFetchCities = jest.fn(
       () =>
         new Promise((resolve) => {
-          resolve(expectedResult);
+          resolve(mockSuccessResult);
         }),
     );
 
@@ -30,5 +29,65 @@ describe("SearchBar", () => {
 
     // Assert
     await waitFor(() => expect(mockFetchCities).toHaveBeenCalledWith(input));
+  });
+
+  test("should render valid options", async () => {
+    // Arrange
+    const input = "Moscow";
+
+    const mockFetchCities = jest.fn(
+      () =>
+        new Promise((resolve) => {
+          resolve(mockSuccessResult);
+        }),
+    );
+
+    fetchCities.mockImplementation(mockFetchCities);
+
+    render(<SearchBar />);
+    const cityInput = screen.getByRole("combobox");
+
+    // Act
+    fireEvent.change(cityInput, {
+      target: { value: input },
+    });
+
+    const option1 = await waitFor(() =>
+      screen.getByText(mockSuccessResult[0].label),
+    );
+    const option2 = await waitFor(() =>
+      screen.getByText(mockSuccessResult[1].label),
+    );
+
+    // Assert
+    expect(option1).toBeInTheDocument();
+    expect(option2).toBeInTheDocument();
+  });
+
+  test("should display no options", async () => {
+    // Arrange
+    const input = "notavalidcity";
+
+    const mockFetchCities = jest.fn(
+      () =>
+        new Promise((resolve) => {
+          resolve([]);
+        }),
+    );
+
+    fetchCities.mockImplementation(mockFetchCities);
+
+    render(<SearchBar />);
+    const cityInput = screen.getByRole("combobox");
+
+    // Act
+    fireEvent.change(cityInput, {
+      target: { value: input },
+    });
+
+    const option = await waitFor(() => screen.getByText("No options"));
+
+    // Assert
+    expect(option).toBeInTheDocument();
   });
 });
