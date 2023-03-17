283516a84a6f0cb4592bb49a6e571f49dffcce9e
Elizabeth <elizaveta.chernosheikina@yandex.ru>
2023-03-17 16:30:36 +0300

    add all themes in BackComponent

--- src/components/BackComponent/BackComponent.jsx
+++ src/components/BackComponent/BackComponent.jsx
@@ -1,11 +1,25 @@
 import { useState } from "react";
 import { ThemeProvider } from "styled-components";
 import WeatherCardComponent from "components/WeatherCardComponent";
-import { rainyTheme, commonStyles } from "global/themes";
+import {
+  rainyTheme,
+  sunnyTheme,
+  cloudyTheme,
+  thunderTheme,
+  snowyTheme,
+  commonStyles,
+} from "global/themes";
 import BackgroundColor from "components/BackComponent/styled";
 
 const BackComponent = () => {
-  const [theme] = useState({ ...rainyTheme, ...commonStyles });
+  const [theme] = useState({
+    ...rainyTheme,
+    ...sunnyTheme,
+    ...cloudyTheme,
+    ...thunderTheme,
+    ...snowyTheme,
+    ...commonStyles,
+  });
 
   return (
     <ThemeProvider theme={theme}>
